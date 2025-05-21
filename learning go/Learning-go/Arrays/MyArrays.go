0e889d025bb32361db4308de890a6793356de066
Adetia <adityaxsetia@gmail.com>
2025-05-21 22:36:16 +0530

    Arrays

---
+++ learning go/Learning-go/Arrays/MyArrays.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello Arrays")
+
+	var fruits [5]string
+	fruits[0] = "Apple"
+	fruits[1] = "Banana"
+	fruits[2] = "Mango"
+	fruits[3] = "Orange"
+	fruits[4] = "Grapes"
+	fmt.Println(fruits)
+	fmt.Println(fruits[0])
+	fmt.Println(fruits[1])
+}
